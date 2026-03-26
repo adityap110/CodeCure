66145f0849e02432d7c1f2a060f11892c0bee8e3
adityap110 <adityapanda25ci@rnsit.ac.in>
2026-03-26 19:04:40 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,6 @@ if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
 # Tell Flask to find the DB in the right place
-os.chdir(project_home)
 
 from app import app, init_db
 init_db()  # creates DB + seeds demo data on first run
